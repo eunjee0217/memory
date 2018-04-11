6f5f09b7053599b6221a390decbf25e0b9d180a9
eunjee0217 <kimej940217@gmail.com>
2018-04-10 17:55:03 -0700

    add

---
+++ CardButton.swift
@@ -0,0 +1,55 @@
+//
+//  CardView.swift
+//  memory
+//
+//  Created by Eunjee_R on 4/3/18.
+//  Copyright © 2018 Elaine Kim. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+@IBDesignable
+class CardButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderThickness: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    
+    @IBInspectable var borderColor :UIColor {
+        get { 
+            if let color = layer.borderColor {
+                return UIColor(cgColor:color)
+            }
+            return UIColor.black
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+}
